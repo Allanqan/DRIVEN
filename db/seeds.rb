f2661b817e2ab83401c716acece6a6ab5a484078
Allan <77512326+Allanqan@users.noreply.github.com>
2026-02-23 16:14:44 +0100

    Fix seeds: remove duplicate transmission key, add demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require "open-uri"
 require "faker"
-puts "clean DB"
+
+puts "Cleaning DB..."
 Booking.destroy_all
 Driver.destroy_all
+User.destroy_all
 
-puts "create at least 10 drivers"
+puts "Creating demo user..."
+demo_user = User.create\!(
+  email: "demo@driven.app",
+  password: "password123",
+  first_name: "Demo",
+  last_name: "User"
+)
 
+puts "Creating drivers..."
 female_pictures = [
   'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651886/szvoreny-attila-Z_DjhwOUvTw-unsplash_b1tcga.jpg',
   'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651451/jake-nackos-IF9TK5Uy-KI-unsplash_pwdezk.jpg',
@@ -29,80 +30,45 @@ male_pictures = [
   'https://res.cloudinary.com/dtwo70uhi/image/upload/v1644651375/ben-parker-OhKElOkQ3RE-unsplash_npnqtl.jpg'
 ]
 
-location = [
+locations = [
   '1 Kingsland Rd, London',
-  '2 Kingsland Rd, London',
   '25 Kingsland Rd, London',
-  '56 Kingsland Rd, London',
-  'Shoreditch',
-  'Brixton',
-  '18 Newtons Yard',
+  'Shoreditch, London',
+  'Brixton, London',
   'Kings Road, London',
   'Sloane Square, London',
-  'Hammersmith'
+  'Hammersmith, London',
+  'Notting Hill, London',
+  'Camden, London',
+  'Hackney, London'
 ]
-female_pictures.each do |female_picture_url|
-  file = URI.open(female_picture_url)
-  female_driver = Driver.new(
+
+female_pictures.each do |url|
+  file = URI.open(url)
+  driver = Driver.new(
     name: Faker::Name.female_first_name,
     license_type: ["A", "B", "C"].sample,
     years_driving: rand(1..8),
     transmission: ["auto", "manual"].sample,
-    transmission: ["auto", "manual"].sample,
-    location: location.sample,
+    location: locations.sample,
     price: rand(10..50)
   )
-  female_driver.photo.attach(io: file, filename: female_picture_url, content_type: 'image/jpg')
-  female_driver.save!
+  driver.photo.attach(io: file, filename: File.basename(url), content_type: 'image/jpg')
+  driver.save\!
 end
 
-male_pictures.each do |male_picture_url|
-  file = URI.open(male_picture_url)
-  male_driver = Driver.new(
+male_pictures.each do |url|
+  file = URI.open(url)
+  driver = Driver.new(
     name: Faker::Name.male_first_name,
     license_type: ["A", "B", "C"].sample,
     years_driving: rand(1..30),
     transmission: ["auto", "manual"].sample,
-    location: location.sample,
+    location: locations.sample,
     price: rand(10..50)
   )
-  male_driver.photo.attach(io: file, filename: male_picture_url, content_type: 'image/jpg')
-  male_driver.save!
+  driver.photo.attach(io: file, filename: File.basename(url), content_type: 'image/jpg')
+  driver.save\!
 end
-puts "10 new drivers created"
 
-# file = URI.open('https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80')
-# john = Driver.new(
-#   name: "John Smith",
-#   license_type: "car",
-#   years_driving: 10,
-#   transmission: "manual",
-#   location: "117 Junction Road, London, N19 5PX",
-#   price: 20
-# )
-# john.photo.attach(io: file, filename: 'john.jpg', content_type: 'image/jpg')
-# john.save!
-
-# file = URI.open('https://res.cloudinary.com/dtwo70uhi/image/upload/v1643981071/edward-cisneros-_H6wpor9mjs-unsplash_rvm7kb.jpg')
-# charlotte = Driver.new(
-#   name: "Charlotte Milliken",
-#   license_type: "car",
-#   years_driving: 17,
-#   transmission: "automatic",
-#   location: "'16 Villa Gaudelet, Paris",
-#   price: 18
-# )
-# charlotte.photo.attach(io: file, filename: 'charlotte.jpg', content_type: 'image/jpg')
-# charlotte.save!
-
-# file = URI.open('https://res.cloudinary.com/dtwo70uhi/image/upload/v1643919468/joseph-gonzalez-iFgRcqHznqg-unsplash_xxvblm.jpg')
-# tobias = Driver.new(
-#   name: "Tobias Weber",
-#   license_type: "HGV",
-#   years_driving: 25,
-#   transmission: "manual",
-#   location: "9 Emser Strasse, Berlin, 12501",
-#   price: 35
-# )
-# tobias.photo.attach(io: file, filename: 'tobias.jpg', content_type: 'image/jpg')
-# tobias.save!
+puts "Done\! #{Driver.count} drivers created. Demo login: demo@driven.app / password123"
